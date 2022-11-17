481cff207be33e189a05d37eea8d1531ad5b87a8
Simon Emms <simon@simonemms.com>
2022-11-17 09:28:39 +0000

    chore: backup

--- controllers/config_controller.go
+++ controllers/config_controller.go
@@ -18,24 +18,40 @@ package controllers
 
 import (
 	"context"
-	"reflect"
+	"time"
 
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	ref "k8s.io/client-go/tools/reference"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	installerv1alpha1 "github.com/mrsimonemms/kubebuilder/api/v1alpha1"
 	"github.com/mrsimonemms/kubebuilder/pkg/resources"
-	"github.com/mrsimonemms/kubebuilder/pkg/rest"
+)
+
+var (
+	jobOwnerKey = ".metadata.name"
+	apiGVStr    = installerv1alpha1.GroupVersion.String()
 )
 
 // ConfigReconciler reconciles a Config object
 type ConfigReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+}
+
+func isJobFinished(job *batchv1.Job) (bool, batchv1.JobConditionType) {
+	for _, c := range job.Status.Conditions {
+		if (c.Type == batchv1.JobComplete || c.Type == batchv1.JobFailed) && c.Status == corev1.ConditionTrue {
+			return true, c.Type
+		}
+	}
+
+	return false, ""
 }
 
 //+kubebuilder:rbac:groups=installer.gitpod.io,resources=configs,verbs=get;list;watch;create;update;patch;delete
@@ -54,146 +70,211 @@ type ConfigReconciler struct {
 func (r *ConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
+	var activeJobs []*batchv1.Job
+	var successfulJobs []*batchv1.Job
+	var failedJobs []*batchv1.Job
+	var mostRecentTime *time.Time // find the last run so we can update the status
+
 	var operatorConfig = &installerv1alpha1.Config{}
 	if err := r.Get(ctx, req.NamespacedName, operatorConfig); err != nil {
-		log.Error(err, "unable to fetch client")
+		log.Error(err, "unable to fetch config")
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
-	operatorConfigOld := operatorConfig.DeepCopy()
-
-	if operatorConfig.Status.InstallerStatus == "" {
-		operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypePending
+	var childJobs batchv1.JobList
+	if err := r.List(ctx, &childJobs, client.InNamespace(req.Namespace), client.MatchingFields{jobOwnerKey: req.Name}); err != nil {
+		log.Error(err, "unable to list child Jobs")
+		return ctrl.Result{}, err
 	}
 
-	log.Info("SJE: " + operatorConfig.Status.InstallerStatus.String())
+	for i, job := range childJobs.Items {
+		_, finishedType := isJobFinished(&job)
+		switch finishedType {
+		case "": // ongoing job
+			activeJobs = append(activeJobs, &childJobs.Items[i])
+		case batchv1.JobFailed:
+			failedJobs = append(failedJobs, &childJobs.Items[i])
+		case batchv1.JobComplete:
+			successfulJobs = append(successfulJobs, &childJobs.Items[i])
+		}
+	}
 
-	switch operatorConfig.Status.InstallerStatus {
-	case installerv1alpha1.InstallerStatusTypePending:
-		operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeRunning
-
-		err := r.Status().Update(context.TODO(), operatorConfig)
+	if mostRecentTime != nil {
+		operatorConfig.Status.LastScheduleTime = &metav1.Time{Time: *mostRecentTime}
+	} else {
+		operatorConfig.Status.LastScheduleTime = nil
+	}
+	operatorConfig.Status.Active = nil
+	for _, activeJob := range activeJobs {
+		jobRef, err := ref.GetReference(r.Scheme, activeJob)
 		if err != nil {
-			log.Error(err, "failed to update client status")
-			return ctrl.Result{}, err
-		} else {
-			log.Info("updated client status: " + operatorConfig.Status.InstallerStatus.String())
-			return ctrl.Result{Requeue: true}, nil
+			log.Error(err, "unable to make reference to active job", "job", activeJob)
+			continue
 		}
-	case installerv1alpha1.InstallerStatusTypeRunning:
-		pod := resources.CreatePod(operatorConfig)
-
-		query := &corev1.Pod{}
-		err := r.Client.Get(ctx, client.ObjectKey{Namespace: pod.Namespace, Name: pod.ObjectMeta.Name}, query)
-		if err != nil && errors.IsNotFound(err) {
-			if operatorConfig.Status.LastPodName == "" {
-				err = ctrl.SetControllerReference(operatorConfig, pod, r.Scheme)
-				if err != nil {
-					return ctrl.Result{}, err
-				}
-
-				err = r.Create(context.TODO(), pod)
-				if err != nil {
-					return ctrl.Result{}, err
-				}
-
-				log.Info("pod created successfully", "name", pod.Name)
-
-				return ctrl.Result{}, nil
-			} else {
-				operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeCleaning
-			}
-		} else if err != nil {
-			log.Error(err, "cannot get pod")
-			return ctrl.Result{}, err
-		} else if query.Status.Phase == corev1.PodFailed ||
-			query.Status.Phase == corev1.PodSucceeded {
-			log.Info("container terminated", "reason", query.Status.Reason, "message", query.Status.Message)
-
-			operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeCleaning
-		} else if query.Status.Phase == corev1.PodRunning {
-			if operatorConfig.Status.LastPodName != operatorConfig.Spec.InstallerImage {
-				if query.Status.ContainerStatuses[0].Ready {
-					log.Info("Trying to bind to: " + query.Status.PodIP)
-
-					if !rest.GetClient(operatorConfig, query.Status.PodIP) {
-						if rest.BindClient(operatorConfig, query.Status.PodIP) {
-							log.Info("Client" /*+ installerConfig.Spec.ClientId*/ + " is binded to pod " + query.ObjectMeta.GetName() + ".")
-							operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeCleaning
-						} else {
-							log.Info("Client not added.")
-						}
-					} else {
-						log.Info("Client binded already.")
-					}
-				} else {
-					log.Info("Container not ready, reschedule bind")
-					return ctrl.Result{Requeue: true}, err
-				}
-
-				log.Info("Client last pod name: " + operatorConfig.Status.LastPodName)
-				log.Info("Pod is running.")
-			}
-		} else if query.Status.Phase == corev1.PodPending {
-			return ctrl.Result{Requeue: true}, nil
-		} else {
-			return ctrl.Result{Requeue: true}, err
-		}
-
-		if !reflect.DeepEqual(operatorConfigOld.Status, operatorConfig.Status) {
-			err = r.Status().Update(context.TODO(), operatorConfig)
-			if err != nil {
-				log.Error(err, "failed to update client status from running")
-				return ctrl.Result{}, err
-			} else {
-				log.Info("updated client status RUNNING -> " + operatorConfig.Status.InstallerStatus.String())
-				return ctrl.Result{Requeue: true}, nil
-			}
-		}
-	case installerv1alpha1.InstallerStatusTypeCleaning:
-		query := &corev1.Pod{}
-		HasClients := rest.HasClients(operatorConfig, query.Status.PodIP)
-
-		err := r.Client.Get(ctx, client.ObjectKey{Namespace: operatorConfig.Namespace, Name: operatorConfig.Status.LastPodName}, query)
-		if err == nil && operatorConfig.ObjectMeta.DeletionTimestamp.IsZero() {
-			if !HasClients {
-				err = r.Delete(context.TODO(), query)
-				if err != nil {
-					log.Error(err, "Failed to remove old pod")
-					return ctrl.Result{}, err
-				} else {
-					log.Info("Old pod removed")
-					return ctrl.Result{Requeue: true}, nil
-				}
-			}
-		}
-
-		if operatorConfig.Status.LastPodName != operatorConfig.Spec.InstallerImage {
-			operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeRunning
-			operatorConfig.Status.LastPodName = operatorConfig.Spec.InstallerImage
-		} else {
-			operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypePending
-			operatorConfig.Status.LastPodName = ""
-		}
-
-		if !reflect.DeepEqual(operatorConfigOld.Status, operatorConfig.Status) {
-			err = r.Status().Update(context.TODO(), operatorConfig)
-			if err != nil {
-				log.Error(err, "failed to update client status from cleaning")
-				return ctrl.Result{}, err
-			} else {
-				log.Info("updated client status CLEANING -> " + operatorConfig.Status.InstallerStatus.String())
-				return ctrl.Result{Requeue: true}, nil
-			}
-		}
+		operatorConfig.Status.Active = append(operatorConfig.Status.Active, *jobRef)
 	}
+
+	log.V(1).Info("job count", "active jobs", len(activeJobs), "successful jobs", len(successfulJobs), "failed jobs", len(failedJobs))
+
+	for _, operatorResource := range resources.CreateResources(operatorConfig) {
+		if err := r.Create(ctx, operatorResource); err != nil {
+			log.Error(err, "unable to create resource for Installer", operatorResource.GetObjectKind().GroupVersionKind().Kind, operatorResource)
+			return ctrl.Result{}, err
+		}
+		log.V(1).Info("created resource for Installer run", operatorResource.GetObjectKind().GroupVersionKind().Kind, operatorResource)
+	}
+
+	// operatorConfigOld := operatorConfig.DeepCopy()
+
+	// if operatorConfig.Status.InstallerStatus == "" {
+	// 	operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypePending
+	// }
+
+	// switch operatorConfig.Status.InstallerStatus {
+	// case installerv1alpha1.InstallerStatusTypePending:
+	// 	operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeRunning
+
+	// 	err := r.Status().Update(context.TODO(), operatorConfig)
+	// 	if err != nil {
+	// 		log.Error(err, "failed to update client status")
+	// 		return ctrl.Result{}, err
+	// 	} else {
+	// 		log.Info("updated client status: " + operatorConfig.Status.InstallerStatus.String())
+	// 		return ctrl.Result{Requeue: true}, nil
+	// 	}
+	// case installerv1alpha1.InstallerStatusTypeRunning:
+	// 	pod := resources.CreatePod(operatorConfig)
+
+	// 	query := &corev1.Pod{}
+	// 	err := r.Client.Get(ctx, client.ObjectKey{Namespace: pod.Namespace, Name: pod.ObjectMeta.Name}, query)
+	// 	if err != nil && errors.IsNotFound(err) {
+	// 		if operatorConfig.Status.LastPodName == "" {
+	// 			err = ctrl.SetControllerReference(operatorConfig, pod, r.Scheme)
+	// 			if err != nil {
+	// 				return ctrl.Result{}, err
+	// 			}
+
+	// 			err = r.Create(context.TODO(), pod)
+	// 			if err != nil {
+	// 				return ctrl.Result{}, err
+	// 			}
+
+	// 			log.Info("pod created successfully", "name", pod.Name)
+
+	// 			return ctrl.Result{}, nil
+	// 		} else {
+	// 			operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeCleaning
+	// 		}
+	// 	} else if err != nil {
+	// 		log.Error(err, "cannot get pod")
+	// 		return ctrl.Result{}, err
+	// 	} else if query.Status.Phase == corev1.PodFailed ||
+	// 		query.Status.Phase == corev1.PodSucceeded {
+	// 		log.Info("container terminated", "reason", query.Status.Reason, "message", query.Status.Message)
+
+	// 		operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeCleaning
+	// 	} else if query.Status.Phase == corev1.PodRunning {
+	// 		if operatorConfig.Status.LastPodName != operatorConfig.Spec.InstallerImage {
+	// 			if query.Status.ContainerStatuses[0].Ready {
+	// 				log.Info("Trying to bind to: " + query.Status.PodIP)
+
+	// 				if !rest.GetClient(operatorConfig, query.Status.PodIP) {
+	// 					if rest.BindClient(operatorConfig, query.Status.PodIP) {
+	// 						log.Info("Client" /*+ installerConfig.Spec.ClientId*/ + " is binded to pod " + query.ObjectMeta.GetName() + ".")
+	// 						operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeCleaning
+	// 					} else {
+	// 						log.Info("Client not added.")
+	// 					}
+	// 				} else {
+	// 					log.Info("Client binded already.")
+	// 				}
+	// 			} else {
+	// 				log.Info("Container not ready, reschedule bind")
+	// 				return ctrl.Result{Requeue: true}, err
+	// 			}
+
+	// 			log.Info("Client last pod name: " + operatorConfig.Status.LastPodName)
+	// 			log.Info("Pod is running.")
+	// 		}
+	// 	} else if query.Status.Phase == corev1.PodPending {
+	// 		return ctrl.Result{Requeue: true}, nil
+	// 	} else {
+	// 		return ctrl.Result{Requeue: true}, err
+	// 	}
+
+	// 	if !reflect.DeepEqual(operatorConfigOld.Status, operatorConfig.Status) {
+	// 		err = r.Status().Update(context.TODO(), operatorConfig)
+	// 		if err != nil {
+	// 			log.Error(err, "failed to update client status from running")
+	// 			return ctrl.Result{}, err
+	// 		} else {
+	// 			log.Info("updated client status RUNNING -> " + operatorConfig.Status.InstallerStatus.String())
+	// 			return ctrl.Result{Requeue: true}, nil
+	// 		}
+	// 	}
+	// case installerv1alpha1.InstallerStatusTypeCleaning:
+	// 	query := &corev1.Pod{}
+	// 	HasClients := rest.HasClients(operatorConfig, query.Status.PodIP)
+
+	// 	err := r.Client.Get(ctx, client.ObjectKey{Namespace: operatorConfig.Namespace, Name: operatorConfig.Status.LastPodName}, query)
+	// 	if err == nil && operatorConfig.ObjectMeta.DeletionTimestamp.IsZero() {
+	// 		if !HasClients {
+	// 			err = r.Delete(context.TODO(), query)
+	// 			if err != nil {
+	// 				log.Error(err, "Failed to remove old pod")
+	// 				return ctrl.Result{}, err
+	// 			} else {
+	// 				log.Info("Old pod removed")
+	// 				return ctrl.Result{Requeue: true}, nil
+	// 			}
+	// 		}
+	// 	}
+
+	// 	if operatorConfig.Status.LastPodName != operatorConfig.Spec.InstallerImage {
+	// 		operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypeRunning
+	// 		operatorConfig.Status.LastPodName = operatorConfig.Spec.InstallerImage
+	// 	} else {
+	// 		operatorConfig.Status.InstallerStatus = installerv1alpha1.InstallerStatusTypePending
+	// 		operatorConfig.Status.LastPodName = ""
+	// 	}
+
+	// 	if !reflect.DeepEqual(operatorConfigOld.Status, operatorConfig.Status) {
+	// 		err = r.Status().Update(context.TODO(), operatorConfig)
+	// 		if err != nil {
+	// 			log.Error(err, "failed to update client status from cleaning")
+	// 			return ctrl.Result{}, err
+	// 		} else {
+	// 			log.Info("updated client status CLEANING -> " + operatorConfig.Status.InstallerStatus.String())
+	// 			return ctrl.Result{Requeue: true}, nil
+	// 		}
+	// 	}
+	// }
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &batchv1.Job{}, jobOwnerKey, func(rawObj client.Object) []string {
+		// grab the job object, extract the owner...
+		job := rawObj.(*batchv1.Job)
+		owner := metav1.GetControllerOf(job)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a CronJob...
+		if owner.APIVersion != apiGVStr || owner.Kind != "CronJob" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&installerv1alpha1.Config{}).
+		Owns(&batchv1.Job{}).
 		Complete(r)
 }
--- pkg/resources/resources.go
+++ pkg/resources/resources.go
@@ -2,27 +2,24 @@ package resources
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 
 	certmanagerv1 "github.com/jetstack/cert-manager/pkg/apis/certmanager/v1"
 	v1 "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/pointer"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	installerv1alpha1 "github.com/mrsimonemms/kubebuilder/api/v1alpha1"
 )
 
 const (
-	nodeVolumeName                   = "node-fs0"
-	nodeVolumeMountPath              = "/mnt/node0"
-	tmpVolumeName                    = "tmp-storage"
-	tmpVolumeMouthPath               = "/tmp"
-	gitpodInstallerSettingsConfigMap = "gitpod-installer-settings"
-	tlsCertName                      = "https-certificates"
+	nodeVolumeName      = "node-fs0"
+	nodeVolumeMountPath = "/mnt/node0"
+	tmpVolumeName       = "tmp-storage"
+	tmpVolumeMouthPath  = "/tmp"
+	tlsCertName         = "https-certificates"
 )
 
 func getLabels(clientResource *installerv1alpha1.Config) map[string]string {
@@ -32,148 +29,153 @@ func getLabels(clientResource *installerv1alpha1.Config) map[string]string {
 	}
 }
 
-func CreateInstallerJob(clientResource *installerv1alpha1.Config) []runtime.Object {
-	return []runtime.Object{
-		&certmanagerv1.Certificate{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      tlsCertName,
-				Namespace: clientResource.Namespace,
-				Labels:    getLabels(clientResource),
+func CreateCertificate(clientResource *installerv1alpha1.Config) *certmanagerv1.Certificate {
+	return &certmanagerv1.Certificate{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      tlsCertName,
+			Namespace: clientResource.Namespace,
+			Labels:    getLabels(clientResource),
+		},
+		Spec: certmanagerv1.CertificateSpec{
+			SecretName: tlsCertName,
+			IssuerRef: v1.ObjectReference{
+				Name: "gitpod-issuer",
+				Kind: "ClusterIssuer",
 			},
-			Spec: certmanagerv1.CertificateSpec{
-				SecretName: tlsCertName,
-				IssuerRef: v1.ObjectReference{
-					Name: "gitpod-issuer",
-					Kind: "ClusterIssuer",
-				},
-				DNSNames: []string{
-					clientResource.Spec.InstallerConfig.Domain,
-					fmt.Sprintf("*.%s", clientResource.Spec.InstallerConfig.Domain),
-					fmt.Sprintf("*.ws.%s", clientResource.Spec.InstallerConfig.Domain),
-				},
+			DNSNames: []string{
+				clientResource.Spec.InstallerConfig.Domain,
+				fmt.Sprintf("*.%s", clientResource.Spec.InstallerConfig.Domain),
+				fmt.Sprintf("*.ws.%s", clientResource.Spec.InstallerConfig.Domain),
 			},
 		},
-		&corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      gitpodInstallerSettingsConfigMap,
-				Namespace: clientResource.Namespace,
-				Labels:    getLabels(clientResource),
-			},
-			Data: map[string]string{
-				// KOTS settings
-				"GITPOD_INSTALLER_CONFIG": fmt.Sprintf("%s/gitpod-config.yaml", tmpVolumeMouthPath),
-				"GITPOD_OBJECTS":          fmt.Sprintf("%s/gitpod", tmpVolumeMouthPath),
+	}
+}
 
-				// General settings
-				"DOMAIN":    clientResource.Spec.InstallerConfig.Domain,
-				"NAMESPACE": clientResource.Namespace,
-
-				// Secret names
-
-				// Database settings
-				"DB_INCLUSTER_ENABLED": "1",
-
-				// Airgap settings
-				"HAS_LOCAL_REGISTRY": "0",
-
-				// Registry settings
-				"REGISTRY_INCLUSTER_ENABLED": "1",
-
-				// Storage settings
-				"STORE_PROVIDER": "incluster",
-
-				// TLS certificate settings
-				"CERT_MANAGER_ENABLED":    "1",
-				"TLS_SELF_SIGNED_ENABLED": "0",
-
-				// User management settings
-				"USER_MANAGEMENT_BLOCK_ENABLED": "0",
-
-				// Advanced settings
-				"ADVANCED_MODE_ENABLED": "0",
-
-				"CUSTOMIZATION_PATCH_ENABLED": "0",
-
-				// Customizations
-				"CONFIG_PATCH":        "",
-				"CUSTOMIZATION_PATCH": "",
-			},
+func CreateConfigMap(clientResource *installerv1alpha1.Config) *corev1.ConfigMap {
+	return &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      clientResource.Name,
+			Namespace: clientResource.Namespace,
+			Labels:    getLabels(clientResource),
 		},
-		&batchv1.Job{
-			ObjectMeta: metav1.ObjectMeta{
-				// Generate a random ID - this might be a bit shit
-				Name: fmt.Sprintf("installer-%d", func() int {
-					rand.Seed(time.Now().UnixNano())
-					min := 1000
-					max := 9999
-					return rand.Intn(max-min) + min
-				}()),
-				Namespace: clientResource.Namespace,
-				Labels:    getLabels(clientResource),
-			},
-			Spec: batchv1.JobSpec{
-				BackoffLimit:            pointer.Int32(1),
-				TTLSecondsAfterFinished: pointer.Int32(0),
-				Template: corev1.PodTemplateSpec{
-					ObjectMeta: metav1.ObjectMeta{
-						Labels: getLabels(clientResource),
-					},
-					Spec: corev1.PodSpec{
-						// ServiceAccountName: "",
-						RestartPolicy: corev1.RestartPolicyOnFailure,
-						Containers: []corev1.Container{
-							{
-								Name:  "installer",
-								Image: clientResource.Spec.InstallerImage,
-								VolumeMounts: []corev1.VolumeMount{
-									{
-										Name:      nodeVolumeName,
-										MountPath: nodeVolumeMountPath,
-										ReadOnly:  true,
-									},
-									{
-										Name:      tmpVolumeName,
-										MountPath: tmpVolumeMouthPath,
-									},
+		Data: map[string]string{
+			// KOTS settings
+			"GITPOD_INSTALLER_CONFIG": fmt.Sprintf("%s/gitpod-config.yaml", tmpVolumeMouthPath),
+			"GITPOD_OBJECTS":          fmt.Sprintf("%s/gitpod", tmpVolumeMouthPath),
+
+			// General settings
+			"DOMAIN":    clientResource.Spec.InstallerConfig.Domain,
+			"NAMESPACE": clientResource.Namespace,
+
+			// Secret names
+
+			// Database settings
+			"DB_INCLUSTER_ENABLED": "1",
+
+			// Airgap settings
+			"HAS_LOCAL_REGISTRY": "0",
+
+			// Registry settings
+			"REGISTRY_INCLUSTER_ENABLED": "1",
+
+			// Storage settings
+			"STORE_PROVIDER": "incluster",
+
+			// TLS certificate settings
+			"CERT_MANAGER_ENABLED":    "1",
+			"TLS_SELF_SIGNED_ENABLED": "0",
+
+			// User management settings
+			"USER_MANAGEMENT_BLOCK_ENABLED": "0",
+
+			// Advanced settings
+			"ADVANCED_MODE_ENABLED": "0",
+
+			"CUSTOMIZATION_PATCH_ENABLED": "0",
+
+			// Customizations
+			"CONFIG_PATCH":        "",
+			"CUSTOMIZATION_PATCH": "",
+		},
+	}
+}
+
+func CreateResources(clientResource *installerv1alpha1.Config) []client.Object {
+	return []client.Object{
+		CreateConfigMap(clientResource),
+		CreateInstallerJob(clientResource),
+	}
+}
+
+func CreateInstallerJob(clientResource *installerv1alpha1.Config) *batchv1.Job {
+	return &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      clientResource.Name,
+			Namespace: clientResource.Namespace,
+			Labels:    getLabels(clientResource),
+		},
+		Spec: batchv1.JobSpec{
+			BackoffLimit:            pointer.Int32(1),
+			TTLSecondsAfterFinished: pointer.Int32(0),
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: getLabels(clientResource),
+				},
+				Spec: corev1.PodSpec{
+					// ServiceAccountName: "",
+					RestartPolicy: corev1.RestartPolicyOnFailure,
+					Containers: []corev1.Container{
+						{
+							Name:  "installer",
+							Image: clientResource.Spec.InstallerImage,
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      nodeVolumeName,
+									MountPath: nodeVolumeMountPath,
+									ReadOnly:  true,
 								},
-								Env: []corev1.EnvVar{
-									{
-										Name:  "MOUNT_PATH",
-										Value: nodeVolumeMountPath,
-									},
+								{
+									Name:      tmpVolumeName,
+									MountPath: tmpVolumeMouthPath,
 								},
-								EnvFrom: []corev1.EnvFromSource{
-									{
-										ConfigMapRef: &corev1.ConfigMapEnvSource{
-											LocalObjectReference: corev1.LocalObjectReference{
-												Name: gitpodInstallerSettingsConfigMap,
-											},
+							},
+							Env: []corev1.EnvVar{
+								{
+									Name:  "MOUNT_PATH",
+									Value: nodeVolumeMountPath,
+								},
+							},
+							EnvFrom: []corev1.EnvFromSource{
+								{
+									ConfigMapRef: &corev1.ConfigMapEnvSource{
+										LocalObjectReference: corev1.LocalObjectReference{
+											Name: clientResource.Name,
 										},
 									},
 								},
-								Command: []string{
-									"/app/scripts/kots-install.sh",
+							},
+							Command: []string{
+								"/app/scripts/kots-install.sh",
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: nodeVolumeName,
+							VolumeSource: corev1.VolumeSource{
+								HostPath: &corev1.HostPathVolumeSource{
+									Path: "/",
+									Type: func() *corev1.HostPathType {
+										r := corev1.HostPathDirectory
+										return &r
+									}(),
 								},
 							},
 						},
-						Volumes: []corev1.Volume{
-							{
-								Name: nodeVolumeName,
-								VolumeSource: corev1.VolumeSource{
-									HostPath: &corev1.HostPathVolumeSource{
-										Path: "/",
-										Type: func() *corev1.HostPathType {
-											r := corev1.HostPathDirectory
-											return &r
-										}(),
-									},
-								},
-							},
-							{
-								Name: tmpVolumeName,
-								VolumeSource: corev1.VolumeSource{
-									EmptyDir: &corev1.EmptyDirVolumeSource{},
-								},
+						{
+							Name: tmpVolumeName,
+							VolumeSource: corev1.VolumeSource{
+								EmptyDir: &corev1.EmptyDirVolumeSource{},
 							},
 						},
 					},
